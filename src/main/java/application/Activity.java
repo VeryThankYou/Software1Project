078f4c22d71d4bfd26466b7c877e0b12f3ffcbc7
Sophus <sophus@stromsborg.dk>
2023-04-17 10:13:34 +0200

    act push

--- src/main/java/application/Activity.java
+++ src/main/java/application/Activity.java
@@ -1,5 +1,36 @@
 package application;
 
-public class Activity {
+public class Activity 
+{
+
+    private String name;
+    private int startDate;
+    private int endDate;
+    private double hourEstimate;
+    private Developer() developer;
+    private Session() session;
+    private int processInfo;
+
+    public void addDev(Developer dev);
+    {
+        // TODO implement here
+    }
+
+    public void computeHoursSpent();
+    {
+        // TODO implement here
+    }
+
+    public void updateInfo(int i);
+    {
+        // TODO implement here
+    }
+
+    public showAvailableDevs();
+    {
+        // TODO implement here
+    }
+
+
     
 }

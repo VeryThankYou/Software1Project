ac3939fefbb7554fe4ea76141a4bd685ad2a58d1
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2023-04-17 12:52:39 +0200

    fixed addActivity

--- src/main/java/application/Project.java
+++ src/main/java/application/Project.java
@@ -29,9 +29,9 @@ public class Project
         }
     }
 
-    private void addActivity()
+    private void addActivity(String newName, int sDate, int eDate, double hourEst)
     {
-        Activity act = new Activity();
+        Activity act = new Activity(newName, sDate, eDate, hourEst);
         activities.add(act);
     }
 

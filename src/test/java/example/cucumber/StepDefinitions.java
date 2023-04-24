a9579d3e029b94ee35c19bd9ca8acc432a602808
Sophus <sophus@stromsborg.dk>
2023-04-24 10:38:06 +0200

    same as before

--- src/test/java/example/cucumber/StepDefinitions.java
+++ src/test/java/example/cucumber/StepDefinitions.java
@@ -8,10 +8,15 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import application.LogPlan;
+import application.Activity;
+import application.Developer;
+import java.time.LocalDate;
 
 public class StepDefinitions {
 
-	private LogPlan logPlan
+	private LogPlan logPlan;
+	private Activity activity;
+	private Developer developer;
 
 	@When("I do nothing")
 	public void iDoNothing() {
@@ -25,7 +30,7 @@ public class StepDefinitions {
 	@Given("there is a developer with id {string} and name {string")
 	public void thereIsADeveloperWithIdAndName(String id, String name)
 	{
-		LogPlan.addDeveloper(id, name)
+		logPlan.addDeveloper(id, name);
 	}
 
 	@When("a developer is added to an activity")
@@ -34,4 +39,28 @@ public class StepDefinitions {
 		
 	}
 	
+	@When("the developer logs the number of hours {double} worked on an activity {Activity}")
+	public void theDeveloperLogsTheNumberOfHoursWorkedOnAnActivity(double hours, Activity activity, LocalDate date)
+	{
+		developer.markHours(activity, date, hours);
+	}
+
+	@Then("the system records the hours {double} worked on the activity {Activity}")
+	public void theSystemRecordsTheHoursWorkedOnTheActivity(double hours, Activity activity)
+	{
+		assertTrue(activity.getActivityCompHours(activity) == hours);
+	}
+
+	@When ("leaves the log hours field empty {null}")
+	public void leaveLogHoursFieldEmpty(LocalDate date)
+	{
+		developer.markHours(activity, date, null);
+	}
+
+	@Then (" the system outputs an error message {string}")
+	public void theSystemOutputsAnErrorMessage(String message)
+	{
+		assertTrue(message == "Error: Hours field is empty");
+	}
+
 }

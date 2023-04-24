8cbf78fa64b1459ee42f4a580e01c356e91479c4
Stromsborg <sophus@stromsborg.dk>
2023-04-24 13:43:40 +0200

    Update StepDefinitions.java

--- src/test/java/example/cucumber/StepDefinitions.java
+++ src/test/java/example/cucumber/StepDefinitions.java
@@ -120,7 +120,6 @@ public class StepDefinitions {
 		assertTrue(activity.getActivityCompHours(activity) == hours);
 	}
 
-	@When ("leaves the log hours field empty {null}")
 	@When ("the developer logs {null} hours worked on the activity")
 	public void leaveLogHoursFieldEmpty(LocalDate date)
 	{

f92d20bee926a0b8ffef76e1271fedf047a9dd6e
ChristianWantzin <chr.wantzin@gmail.com>
2023-04-28 13:29:29 +0200

    logIN fxml added

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -13,6 +13,12 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 public class App extends Application 
 {
     public static void main(String[] args) throws FileNotFoundException, IOException
@@ -52,5 +58,23 @@ public class App extends Application
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+
+/*@Override
+public void start(Stage primaryStage) {
+    Button btn = new Button();
+    btn.setText("Say 'Hello World'");
+    btn.setOnAction(event -> System.out.println("Hello World!"));
+
+    StackPane root = new StackPane();
+    root.getChildren().add(btn);
+
+    Scene scene = new Scene(root, 300, 250);
+
+    primaryStage.setTitle("My JavaFX App");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+}
+*/
 
 }
